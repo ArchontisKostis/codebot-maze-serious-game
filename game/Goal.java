eddd6e41607dfa44453501c806b025c4b72b625a
Archontis E Kostis <arxontisk02@gmail.com>
2026-05-28 20:31:35 +0300

    enhance: make goal tile have pixel art image

--- game/Goal.java
+++ game/Goal.java
@@ -1,18 +1,12 @@
 import greenfoot.Actor;
-import greenfoot.Color;
 import greenfoot.GreenfootImage;
 
 public class Goal extends Actor {
 
     public Goal() {
+        GreenfootImage img = new GreenfootImage("ui/goal-tile.png");
         int size = GameAreaConfig.TILE_SIZE_PX;
-        GreenfootImage image = new GreenfootImage(size, size);
-        image.setColor(new Color(70, 160, 90));
-        image.fill();
-        image.setColor(new Color(35, 95, 45));
-        image.drawRect(0, 0, size - 1, size - 1);
-        image.setColor(Color.WHITE);
-        image.drawString("G", size / 3, (size * 2) / 3);
-        setImage(image);
+        img.scale(size, size);
+        setImage(img);
     }
 }
